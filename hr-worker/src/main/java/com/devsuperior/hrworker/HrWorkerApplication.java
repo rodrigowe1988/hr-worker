a87795523bec65e2b5cf3ebbd1024350aea90bbc
Rodrigo Weber <CIANDT\rodrigoweber@lnb017003cps.ciandt.global>
2022-05-02 17:56:51 -0300

    implementando getAll e getById

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HrWorkerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
 
